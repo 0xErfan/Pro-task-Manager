4a968442c40805fb16a53ce36901bd3ee69bb318
Erfan <eftekharierfan5555555555@gmail.com>
2024-03-13 21:21:27 +0330

    add user tasks component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,83 +5,83 @@ import { isLoginSetter } from '../Redux/Futures/userSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { CgSortAz } from "react-icons/cg";
 import { useNavigate } from 'react-router-dom'
-import { FaRegCircle } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
 import Nav from '../components/Nav'
-import { categoryList } from '../components/AddTodo'
 import AddTodo from '../components/AddTodo'
 import OverlayFilter from '../components/OverlayFilter'
+import Task from '../components/Task'
 
 export default function Home() {
 
     const dispatch = useDispatch()
     const { isLogin, userData } = useSelector(state => state.user)
     const navigate = useNavigate()
+    const [shownTodos, setShownTodos] = useState([])
     const [addTodoShown, setAddTodoShown] = useState(false)
 
     useEffect(() => { dispatch(isLoginSetter(getCookie())) }, [])
+    useEffect(() => { setShownTodos(allUserTodos), console.log("setShown") }, [userData.todos])
+
+
+    let allUserTodos = userData.todos
+    allUserTodos = allUserTodos.map(todo => typeof todo != "string" ? todo : JSON.parse(todo))
+
+    const taskFilterHandler = e => {
+        let filteredTodos = [...allUserTodos]
+        filteredTodos = filteredTodos.filter(todo => [todo.title.toLowerCase(), todo.description.toLowerCase()].some(text => text.includes(e.target.value.toLowerCase())))
+        setShownTodos(filteredTodos)
+    }
 
     return (
         <main>
             {
-                !isLogin ? <Intro /> : (
-                    <section className='container'>
-                        <div className='flex items-center justify-between my-6'>
-                            <CgSortAz className='size-8 cursor-pointer' />
-                            <h2 className='text-xl font-bold font-lato-bold text-primary'>UpTodo</h2>
+                !isLogin ?
+                    <Intro />
+                    :
+                    <>
+                        <section className='container h-screen'>
+                            <div className='flex items-center justify-between my-6'>
+                                <CgSortAz className='size-8 cursor-pointer' />
+                                <h2 className='text-xl font-bold font-lato-bold text-primary'>UpTodo</h2>
+                                {
+                                    !userData?.userImg ?
+                                        <img onClick={() => navigate("/account")} className=' cursor-pointer size-12 object-cover rounded-full' src="https://fakeimg.pl/250x100/" alt="Profile" />
+                                        :
+                                        <div className='flex items-center justify-center size-12 rounded-full bg-dark-light text-xl font-bold'>{userData.name[0]}</div>
+                                }
+                            </div>
                             {
-                                !userData?.userImg ?
-                                    <img onClick={() => navigate("/account")} className=' cursor-pointer size-12 object-cover rounded-full' src="https://fakeimg.pl/250x100/" alt="Profile" />
-                                    :
-                                    <div className='flex items-center justify-center size-12 rounded-full bg-dark-light text-xl font-bold'>{userData.name[0]}</div>
-                            }
-                        </div>
-                        {
-                            userData?.todos.length ?
-                                <>
-                                    <div className=''>
-                                        <div className='flex gap-2 items-center h-12 rounded-[px] bg-dark-light px-3 border border-border/30'>
-                                            <IoSearch className='size-6' />
-                                            <input className='outline-none w-full h-full bg-transparent' placeholder='Search for your task...' type="text" />
+                                allUserTodos?.length ?
+                                    <>
+                                        <div className=''>
+                                            <div className='flex gap-2 items-center h-12 rounded-[px] bg-dark-light px-3 border border-border/30'>
+                                                <IoSearch className='size-6' />
+                                                <input onChange={taskFilterHandler} className='outline-none w-full h-full bg-transparent' placeholder='Search for your task...' type="text" />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <select defaultValue="all" className=' flex items-center justify-center p-2 outline-none bg-primary-gray mt-4 w-28 h-10 rounded-md' onChange={e => console.log(e.target.value)}>
-                                        <option value="all">All</option>
-                                        <option value="today">Today</option>
-                                        <option value="tomorrow">Tomorrow</option>
-                                    </select>
-                                    <div className='space-y-4 mt-4'>
-                                        {
-                                            userData.todos.map(data => (
-                                                <div key={data.id} className='w-full h-[72px] p-2 bg-primary-gray rounded-md' >
-                                                    <div className='flex items-center gap-3'>
-                                                        <FaRegCircle className='size-5' />
-                                                        <div>
-                                                            <h4 className=' font-lato-bold text-xl'>{data.title}</h4>
-                                                            <p className=' text-milky-dark '>{data.description}</p>
-                                                        </div>
-                                                        <div className='flex items-center justify-start gap-2 ml-auto'>
-                                                            {data.category && <div
-                                                                className='p-1 rounded-sm'
-                                                                style={{ backgroundColor: categoryList.find(cat => cat.catName == data.category).bgColor }} >{data.category}</div>}
-                                                        </div>
-                                                        {data.priority && <div>{data.priority}</div>}
-                                                    </div>
-                                                </div>))
-                                        }
-                                    </div>
-                                </>
-                                :
-                                <div className='mt-40'>
-                                    <img className='block m-auto max-s-[277px] s-full' src="/Images/Checklist-rafiki 1.png" />
-                                    <h5 className=' text-[20px] font-bold text-center mt-2'>What do you want to do today?</h5>
-                                    <p className='text-center text-md text-milky mt-2'>Tap + to add your tasks</p>
-                                </div>
-                        }
+                                        <select defaultValue="all" className=' flex items-center justify-center p-2 outline-none bg-primary-gray mt-4 w-28 h-10 rounded-md' onChange={e => console.log(e.target.value)}>
+                                            <option value="all">All</option>
+                                            <option value="today">Today</option>
+                                            <option value="tomorrow">Tomorrow</option>
+                                        </select>
+                                        <div className='space-y-4 mt-4'>
+                                            {// 1000 because if the todo did't have priority value, it goes to the bottom in sorting
+                                                [...shownTodos].sort((a, b) => (a.priority || 10000) - (b.priority || 10000)).map(data => <Task {...data} />)
+                                            }</div>
+                                    </>
+                                    :
+                                    !shownTodos.length ? <div>No task found</div>
+                                        :
+                                        <div className='mt-40'>
+                                            <img className='block m-auto max-s-[277px] s-full' src="/Images/Checklist-rafiki 1.png" />
+                                            <h5 className=' text-[20px] font-bold text-center mt-2'>What do you want to do today?</h5>
+                                            <p className='text-center text-md text-milky mt-2'>Tap + to add your tasks</p>
+                                        </div>
+                            }
+                            <AddTodo visible={addTodoShown} />
+                        </section>
                         <Nav />
-                        <AddTodo visible={addTodoShown} />
-                    </section>
-                )
+                    </>
             }
             <OverlayFilter />
         </main>
---
+++ src/components/Task.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { categoryList } from './AddTodo'
+import { FaRegCircle } from "react-icons/fa";
+import { HiOutlineFlag } from "react-icons/hi2";
+
+export default function Task({ id, description, title, category, priority }) {
+
+    return (
+        <div key={id} className='w-full py-2 px-3 justify-between h-24 relative bg-primary-gray rounded-md' >
+            <div className='flex flex-col items-center gap-3'>
+                <div className='flex items-center gap-3 w-full relative'>
+                    <FaRegCircle className='size-5 absolute top-1/2 inset-0 h-full translate-y-1/2' />
+                    <div className='ml-auto w-[87%] relative'>
+                        <h4 className='font-lato-bold text-xl truncate-letters w-full]'>{title}</h4>
+                        <p className=' text-milky-dark absolute top-[100%] w-[44%] truncate-letters translate-y-1/2 line-clamp-1 '>{description}</p>
+                    </div>
+                </div>
+
+                <div className='ml-auto'>
+                    <div className='flex justify-between'>
+                        <div className='flex justify-between gap-1'>
+                            <div className='flex items-center gap-2 justify-between'>
+                                {category && <div
+                                    className='h-9 flex items-center justify-center rounded-[4px] text-white'
+                                    style={{ backgroundColor: categoryList.find(cat => cat.catName == category).bgColor }}>
+                                    <div className='flex items-center gap-2 px-1'>
+                                        <div className='ch:size-[22px]'>{categoryList.find(cat => cat.catName == category).svg}</div>
+                                        {category}
+                                    </div>
+                                </div>}
+                            </div>
+                            {priority && <div>
+                                <div className='h-9 flex items-center justify-center rounded-[4px] text-white border-[2.5px] px-2 gap-2 border-primary'>
+                                    <HiOutlineFlag />
+                                    {priority}
+                                </div>
+                            </div>}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>)
+}
